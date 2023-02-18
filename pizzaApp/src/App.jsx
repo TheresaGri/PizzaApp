b598e25d819dac50646e6dc6549a3fac908dd0c6
hansdav <hdavidson21@gmail.com>
2023-02-18 16:37:38 +0100

    fixed pizzas state from null to array

--- pizzaApp/src/App.jsx
+++ pizzaApp/src/App.jsx
@@ -1,38 +1,36 @@
-
-import { useEffect, useState } from "react";
-import "./App.css";
+import { useEffect, useState } from 'react';
+import './App.css';
 import Header from './components/header';
 import banner from './assets/banner.webp';
 
 async function getPizzas() {
-  const url = "http://localhost:3000/api/pizzas";
-  const res = await fetch(url);
-  const data =await  res.json();
-  return data;
+	const url = 'http://localhost:3000/api/pizzas';
+	const res = await fetch(url);
+	const data = await res.json();
+	return data;
 }
 
 function App() {
-  const [pizzas, setPizzas] = useState(null);
+	const [pizzas, setPizzas] = useState([]);
 
-useEffect(() => {
-  async function showPizzas()  {
-    let data = await getPizzas();
-    setPizzas(data)
-  }
-  console.log(pizzas);
-
-},[pizzas]);
-  return (
-    <div className="homepage">
-      {pizzas.map((pizza ) => (
-        <li>{pizza.name}</li>
-      )
-      
-      )}
-      <Header> </Header>
-			<img id='restaurant-banner' src={banner}></img>
-    </div>
-  );
+	useEffect(() => {
+		async function showPizzas() {
+			let data = await getPizzas();
+			setPizzas(data);
+		}
+		console.log(pizzas);
+	}, [pizzas]);
+	return (
+		<>
+			<div className='homepage'>
+				{pizzas.map((pizza) => (
+					<li>{pizza.name}</li>
+				))}
+				<Header> </Header>
+				<img id='restaurant-banner' src={banner}></img>
+			</div>
+		</>
+	);
 }
 
 export default App;
