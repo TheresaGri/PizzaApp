e335caecd3e6dc89d04fb34aecbfdd87c2af9ae8
hansdav <hdavidson21@gmail.com>
2023-02-18 16:28:21 +0100

    homepage styling with header and banner

--- pizzaApp/src/App.jsx
+++ pizzaApp/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Header from './components/header';
+import banner from './assets/banner.webp';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-    </div>
-  )
+	return (
+		<div className='homepage'>
+			<Header> </Header>
+			<img id='restaurant-banner' src={banner}></img>
+		</div>
+	);
 }
 
-export default App
+export default App;
