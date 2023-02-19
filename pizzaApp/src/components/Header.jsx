24e00dbcd9509ebfca5897a571478cae8c1bf252
Theresa Grimus <T.Grimus@gmx.net>
2023-02-19 18:19:17 +0100

    when menu is clicked it scrolls down to the menu

--- pizzaApp/src/components/Header.jsx
+++ pizzaApp/src/components/Header.jsx
@@ -1,23 +1,31 @@
-import './Header.css';
-import Badge from './Badge';
+import "./Header.css";
+import Badge from "./Badge";
+import { forwardRef } from "react";
 
-export default function Header() {
-	return (
-		<>
-			<div id='header'>
-				<div className='header-div' id='header-home' >
-					Home
-				</div>
-				<div className='header-div' id='header-menu'>
-					Menu
-				</div>
-				<div className='header-div' id='header-about'>
-					About
-				</div>
-				<Badge value='5'>
-					<span id='shopping-cart'>🛒</span>
-				</Badge>
-			</div>
-		</>
-	);
+function Header({ onclick }, ref) {
+  return (
+    <div id="header">
+      <div className="header-div" id="header-home">
+        Home
+      </div>
+      <div
+        ref={ref}
+        className="header-div"
+        id="header-menu"
+        onClick={() => {
+          onclick();
+        }}
+      >
+        Menu
+      </div>
+      <div className="header-div" id="header-about">
+        About
+      </div>
+      <Badge value="5">
+        <span id="shopping-cart">🛒</span>
+      </Badge>
+    </div>
+  );
 }
+
+export default forwardRef(Header);
