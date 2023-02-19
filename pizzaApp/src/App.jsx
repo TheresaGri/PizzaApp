db3d03104129e8c9094f9357e7162d227cb09543
hansdav <hdavidson21@gmail.com>
2023-02-19 16:41:10 +0100

    header done

--- pizzaApp/src/App.jsx
+++ pizzaApp/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/header';
-import banner from './assets/banner.webp';
+import banner from './assets/pizza-banner.jpeg';
 
 async function getPizzas() {
 	const url = 'http://localhost:3000/api/pizzas';
@@ -26,8 +26,17 @@ function App() {
 				{pizzas.map((pizza) => (
 					<li>{pizza.name}</li>
 				))}
-				<Header> </Header>
-				<img id='restaurant-banner' src={banner}></img>
+				<div id='banner'>
+					<Header> </Header>
+					<div id='header-text'>
+						<h3 id='header-intro'>
+							When the moon hits your eye like a big pizza pie...
+						</h3>
+						<h1 id='header-tagline'>That's Amore Pizza</h1>
+						<h2 id='reserve-table'>Reserve Table</h2>
+						<h2 id='browse-menu'>Browse Menu</h2>
+					</div>
+				</div>
 			</div>
 		</>
 	);
