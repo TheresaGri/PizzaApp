fc80b750ab98ca82c1828726f77a993b7da8241f
Theresa Grimus <T.Grimus@gmx.net>
2023-02-19 17:56:00 +0100

    updated filtered pizzas

--- pizzaApp/src/App.jsx
+++ pizzaApp/src/App.jsx
@@ -1,23 +1,17 @@
-
-import { useEffect, useState } from 'react';
-import './App.css';
-import Header from './components/header';
-import banner from './assets/pizza-banner.jpeg';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getPizzas } from "./data/data";
+import { getPizzaByName } from "./data/data";
+import Header from "./components/Header";
+import banner from "./assets/pizza-banner.jpeg";
 import Filter from "./components/Filter";
 
-async function getPizzas() {
-	const url = 'http://localhost:3000/api/pizzas';
-	const res = await fetch(url);
-	const data = await res.json();
-	return data;
-}
-
 function App() {
-	const [pizzas, setPizzas] = useState([]);
+  const [pizzas, setPizzas] = useState([]);
   const [name, setName] = useState("");
   const [filteredPizza, setFilteredPizza] = useState([]);
 
-	 useEffect(() => {
+  useEffect(() => {
     async function loadPizzas() {
       let data = await getPizzas();
       setPizzas(data);
@@ -29,34 +23,38 @@ function App() {
     setName(event.target.value);
   }
 
-  useEffect(()=> {
+  useEffect(() => {
     async function loadFilteredPizzas(name) {
       let data = await getPizzaByName(name);
-      setFilteredPizza(data)
+      setFilteredPizza(data);
     }
     loadFilteredPizzas(name);
+  }, [name]);
 
-  },[name])
-	return (
-		<>
-			<div className='homepage'>
-				{pizzas.map((pizza) => (
-					<li>{pizza.name}</li>
-				))}
-				<div id='banner'>
-					<Header> </Header>
-					<div id='header-text'>
-						<h3 id='header-intro'>
-							When the moon hits your eye like a big pizza pie...
-						</h3>
-						<h1 id='header-tagline'>That's Amore Pizza</h1>
-						<h2 id='reserve-table'>Reserve Table</h2>
-						<h2 id='browse-menu'>Browse Menu</h2>
-					</div>
-				</div>
-        <Filter name={name} onChange={filterPizzas}></Filter>
-			</div>
-		</>
-	);
+  return (
+    <div className="homepage">
+      {pizzas.map((pizza) => (
+        <li key={pizza.id}>{pizza.name}</li>
+      ))}
+      <div id="banner">
+        <Header> </Header>
+        <div id="header-text">
+          <h3 id="header-intro">
+            When the moon hits your eye like a big pizza pie...
+          </h3>
+          <h1 id="header-tagline">That's Amore Pizza</h1>
+          <h2 id="reserve-table">Reserve Table</h2>
+          <h2 id="browse-menu">Browse Menu</h2>
+        </div>
+      </div>
+      <Filter name={name} onChange={filterPizzas}></Filter>
+      <div>
+        {filteredPizza.map((pizza) => (
+          <li key={pizza.id}>{pizza.name}</li>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default App;
