c1b723eb47b335e5b54ccd24bede672e8b007b44
Theresa Grimus <T.Grimus@gmx.net>
2023-02-21 22:07:04 +0100

    tried to fix delete and add button functionality, changed some code snippets to shorter ones

--- pizzaApp/src/data/data.jsx
+++ pizzaApp/src/data/data.jsx
@@ -12,16 +12,11 @@ export async function getPizzaByName(name) {
   return data;
 } */
 
-export async function getPizzaByNamePriceAndAllergen(name, maxPrice, allergen) {
-  const url = `http://localhost:3000/api/pizzas?max-price=${maxPrice}&name=${name}&avoid-allergen-by-name=${allergen}`;
+export async function getPizzaByNamePriceAndAllergen(name, maxPrice, allergen, sort) {
+  const url = `http://localhost:3000/api/pizzas?max-price=${maxPrice}&name=${name}&avoid-allergen-by-name=${allergen}&sort-asc=${sort} `;
   const res = await fetch(url);
   const data = await res.json();
   return data;
 }
 
-export async function getPizzaById(id) {
-  const url = `http://localhost:3000/api/pizzas/${id}`;
-  const res = await fetch(url);
-  const data = await res.json();
-  return data;
-}
+
