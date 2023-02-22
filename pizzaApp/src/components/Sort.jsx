c8367aa4c2f05b1a1f97123b36ad4e974b62502c
hansdav <hdavidson21@gmail.com>
2023-02-22 18:06:25 +0100

    changes

--- pizzaApp/src/components/Sort.jsx
+++ pizzaApp/src/components/Sort.jsx
@@ -1,11 +1,10 @@
 import Button from './Button';
 
-export default function Sorting(props){
-  return (
-    <div className="soritng-bar">
-      <Button onClick={props.onSort1}>sort ba name</Button>
-      <Button onClick={props.onSort2}>sort by price</Button>
-    </div>
-
-  );
+export default function Sorting(props) {
+	return (
+		<div className='sorting-bar'>
+			<Button onClick={props.onSort1}>sort ba name</Button>
+			<Button onClick={props.onSort2}>sort by price</Button>
+		</div>
+	);
 }
