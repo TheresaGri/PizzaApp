2397dd7a3e7e2f09459bd56f406b65eb858e81cd
hansdav <hdavidson21@gmail.com>
2023-02-25 17:45:57 +0100

    finalized css

--- pizzaApp/src/components/ActiveOrder.jsx
+++ pizzaApp/src/components/ActiveOrder.jsx
@@ -1,27 +1,29 @@
-import "./ActiveOrder.css";
+import './ActiveOrder.css';
 
 export default function ActiveOrder(props) {
-  let combineOrderAmount = props.array;
-  let orderTotal = props.total;
-  return (
-    <div id="active-order">
-      <h1 id="order-title">Your order</h1>
-      <hr id="order-line"></hr>
-      <div id="order-list">
-        {combineOrderAmount.map((order) =>
-          order.amount > 0 ? (
-            <div className="order-item" key={order.name}>
-              <div className="item-name">{order.name}</div>
-              <div className="item-price">€{order.price * order.amount}.00</div>
-              <span className="item-amount">Amount: {order.amount}</span>
-            </div>
-          ) : null
-        )}
-      </div>
-      <div id="order-total">
-        <span>Total: </span>
-        <span>€{orderTotal}.00</span>
-      </div>
-    </div>
-  );
+	let combineOrderAmount = props.array;
+	let orderTotal = props.total;
+	return (
+		<div id='active-order'>
+			<h1 id='active-order-title'>Your order</h1>
+			<hr id='active-order-line'></hr>
+			<div id='active-order-list'>
+				{combineOrderAmount.map((order) =>
+					order.amount > 0 ? (
+						<div className='active-order-item' key={order.name}>
+							<div className='active-item-name'>{order.name}</div>
+							<div className='active-item-price'>
+								€{order.price * order.amount}.00
+							</div>
+							<span className='active-item-amount'>Amount: {order.amount}</span>
+						</div>
+					) : null
+				)}
+			</div>
+			<div id='active-order-total'>
+				<span>Total: </span>
+				<span>€{orderTotal}.00</span>
+			</div>
+		</div>
+	);
 }
